883122b8f6a374b0d22fb768555aee9209a708fe
oshingate <oshingatae@gmail.com>
2021-01-29 12:06:05 +0530

    final change

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-error-page',
-  templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.css']
+  selector: "app-error-page",
+  templateUrl: "./error-page.component.html",
+  styleUrls: ["./error-page.component.css"],
 })
 export class ErrorPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  gotoHome() {
+    this.router.navigate(["/", "login"]);
   }
-
 }
